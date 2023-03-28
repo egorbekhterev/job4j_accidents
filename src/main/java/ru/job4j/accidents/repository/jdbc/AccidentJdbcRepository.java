2548104930bd74511e37dad1f1a4c715ca7094ca
Paranoj <e.behterev@yandex.ru>
2023-03-28 22:02:44 +0700

    updated select methods for jdbcRepository

--- src/main/java/ru/job4j/accidents/repository/jdbc/AccidentJdbcRepository.java
+++ src/main/java/ru/job4j/accidents/repository/jdbc/AccidentJdbcRepository.java
@@ -5,12 +5,14 @@ import net.jcip.annotations.ThreadSafe;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 import ru.job4j.accidents.model.Accident;
+import ru.job4j.accidents.model.AccidentType;
 import ru.job4j.accidents.model.Rule;
 import ru.job4j.accidents.repository.AccidentRepository;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.*;
 
 /**
@@ -27,37 +29,79 @@ public class AccidentJdbcRepository implements AccidentRepository {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(AccidentJdbcRepository.class.getName());
 
-    private AccidentTypeJdbcRepository accidentTypeJdbcRepository;
+    private static final String SQL_FIND_BY_ID =
+            "select a.id, a.name, a.text, a.address, a.car_number, "
+                    + "a.type_id, t.name as Type_Name, ar.rule_id, r.name as Rule_Name "
+                    + "from accidents a "
+                    + "left join types t on a.type_id = t.id "
+                    + "left join accidents_rules ar on a.id = ar.accident_id "
+                    + "left join rules r on ar.rule_id = r.id "
+                    + "where a.id = ?;";
 
-    private RuleJdbcRepository ruleJdbcRepository;
+    private static final String SQL_FIND_ALL =
+            "select a.id, a.name, a.text, a.address, a.car_number, "
+                    + "a.type_id, t.name as Type_Name, ar.rule_id, r.name as Rule_Name "
+                    + "from accidents a "
+                    + "left join types t on a.type_id = t.id "
+                    + "left join accidents_rules ar on a.id = ar.accident_id "
+                    + "left join rules r on ar.rule_id = r.id "
+                    + "ORDER BY a.id;";
 
-    /**
-     * Получаем множество уникальных значений {@link Rule} из внешней таблицы @ManyToMany accidents_rules.
-     * @param id идентификатор
-     * @return множество уникальных значений {@link Rule}
-     */
-    private Set<Rule> findRulesByAccidentId(int id) {
-        return new HashSet<>(jdbc.query("select rule_id from accidents_rules where accident_id = ?",
-                (resultSet, rowNum) -> {
-                    Rule rule = new Rule();
-                    rule.setId(resultSet.getInt("rule_id"));
-                    rule.setName(ruleJdbcRepository
-                            .findRuleById(resultSet.getInt("rule_id")).get().getName());
-                    return rule;
-                }, id));
+    private Accident extractAccident(ResultSet rs) throws SQLException {
+        var accident = new Accident();
+        accident.setId(rs.getInt("id"));
+        accident.setName(rs.getString("name"));
+        accident.setText(rs.getString("text"));
+        accident.setAddress(rs.getString("address"));
+        accident.setCarNumber(rs.getString("car_number"));
+        accident.setType(
+                new AccidentType(rs.getInt("type_id"), rs.getString("Type_Name")));
+        accident.setRules(new HashSet<>());
+        return accident;
     }
 
-    private final RowMapper<Accident> accidentRowMapper = (resultSet, rowNum) -> {
-        Accident accident = new Accident();
-        accident.setId(resultSet.getInt("id"));
-        accident.setName(resultSet.getString("name"));
-        accident.setText(resultSet.getString("text"));
-        accident.setAddress(resultSet.getString("address"));
-        accident.setCarNumber(resultSet.getString("car_number"));
-        accident.setType(accidentTypeJdbcRepository.findTypeById(resultSet.getInt("type_id")).get());
-        accident.setRules(findRulesByAccidentId(resultSet.getInt("id")));
-        return accident;
-    };
+    private void extractRules(Accident accident, ResultSet rs) throws SQLException {
+        var ruleId = rs.getInt("rule_id");
+        accident.getRules().add(new Rule(ruleId, rs.getString("Rule_Name")));
+    }
+
+    @Override
+    public List<Accident> findAll() {
+        return jdbc.query(SQL_FIND_ALL, rs -> {
+            Map<Integer, Accident> map = new HashMap<>();
+            List<Accident> accidents = new ArrayList<>();
+            while (rs.next()) {
+                var accidentId = rs.getInt("id");
+                var accident = map.get(accidentId);
+                if (accident == null) {
+                    accident = extractAccident(rs);
+                    map.putIfAbsent(accidentId, accident);
+                    accidents.add(accident);
+                }
+                extractRules(accident, rs);
+            }
+            return accidents;
+        });
+    }
+
+    @Override
+    public Optional<Accident> findById(int id) {
+        return jdbc.query(SQL_FIND_BY_ID, rs -> {
+            Map<Integer, Accident> map = new HashMap<>();
+            List<Accident> accidents = new ArrayList<>();
+            while (rs.next()) {
+                var accidentId = rs.getInt("id");
+                var accident = map.get(accidentId);
+                if (accident == null) {
+                    accident = extractAccident(rs);
+                    map.putIfAbsent(accidentId, accident);
+                    accidents.add(accident);
+                }
+                extractRules(accident, rs);
+            }
+            return Optional.ofNullable(accidents.get(0));
+        }, id);
+    }
 
     @Override
     public Optional<Accident> save(Accident accident) {
@@ -83,19 +127,6 @@ public class AccidentJdbcRepository implements AccidentRepository {
         return rsl;
     }
 
-    @Override
-    public List<Accident> findAll() {
-        return jdbc.query("select id, name, text, address, car_number, type_id from accidents", accidentRowMapper);
-    }
-
-    @Override
-    public Optional<Accident> findById(int id) {
-        var accident = jdbc.queryForObject(
-                "select id, name, text, address, car_number, type_id from accidents where id = ?",
-                accidentRowMapper, id);
-        return Optional.ofNullable(accident);
-    }
-
     private boolean deleteFromAccidentsRulesById(int id) {
         var affectedRows = jdbc.update("delete from accidents_rules where accident_id = ?", id);
         return affectedRows > 0;
@@ -103,11 +134,13 @@ public class AccidentJdbcRepository implements AccidentRepository {
 
     @Override
     public boolean update(Accident accident) {
-        var affectedRows = jdbc.update("update accidents set name = ?, text = ?, address = ?, car_number = ?, type_id = ? "
-                + "where id = ?", accident.getName(), accident.getText(), accident.getAddress(),
-                accident.getCarNumber(), accident.getType().getId(), accident.getId());
+        if (!deleteFromAccidentsRulesById(accident.getId())) {
+            return false;
+        }
 
-        deleteFromAccidentsRulesById(accident.getId());
+        var affectedRows = jdbc.update("update accidents set name = ?, text = ?, address = ?, car_number = ?, type_id = ? "
+                        + "where id = ?", accident.getName(), accident.getText(), accident.getAddress(),
+                accident.getCarNumber(), accident.getType().getId(), accident.getId());
 
         accident.getRules().forEach(rule -> jdbc.update("insert into accidents_rules (accident_id, rule_id) "
                 + "values (?, ?)", accident.getId(), rule.getId()));
