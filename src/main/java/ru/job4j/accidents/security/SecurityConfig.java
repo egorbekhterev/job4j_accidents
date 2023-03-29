a5eed2909acce9af4e1a8b7a244532cbb53e3ebb
Paranoj <e.behterev@yandex.ru>
2023-03-29 22:48:27 +0700

    1. Авторизация через JDBC [#2094]

--- src/main/java/ru/job4j/accidents/security/SecurityConfig.java
+++ src/main/java/ru/job4j/accidents/security/SecurityConfig.java
@@ -7,8 +7,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.sql.DataSource;
 
 /**
  * @author: Egor Bekhterev
@@ -20,15 +23,13 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
-    PasswordEncoder passwordEncoder;
+    DataSource ds;
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .passwordEncoder(passwordEncoder)
-                .withUser("user").password(passwordEncoder.encode("123456")).roles("USER")
-                .and()
-                .withUser("admin").password(passwordEncoder.encode("123456")).roles("USER", "ADMIN");
+        auth.jdbcAuthentication()
+                .dataSource(ds).withUser(User.withUsername("user").password(
+                        passwordEncoder().encode("123456")).roles("USER"));
     }
 
     @Bean
