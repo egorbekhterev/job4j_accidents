4f26435e756146b6aa1c46d122f2ab653926efb3
Paranoj <e.behterev@yandex.ru>
2023-03-29 22:24:08 +0700

    0. Spring Security [#6879]

---
+++ src/main/java/ru/job4j/accidents/security/SecurityInit.java
@@ -0,0 +1,11 @@
+package ru.job4j.accidents.security;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+/**
+ * @author: Egor Bekhterev
+ * @date: 29.03.2023
+ * @project: job4j_accidents
+ */
+public class SecurityInit extends AbstractSecurityWebApplicationInitializer {
+}
